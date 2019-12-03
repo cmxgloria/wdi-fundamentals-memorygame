2ced7b09570bb9c4a9c7ba4cb6015082cdd6db04
Gloria Chen <cmxgloria@gmail.com>
2019-12-03 19:29:42 +1100

    updated

--- main.js
+++ main.js
@@ -1,34 +1,10 @@
-<html>
-  <link rel="stylesheet" text="text/css" href="css/style.css">
 
-    <head>
-      <header>
-        <h1>Memory Card Game</h1>
-</head>
-      <title>
-        Memory Card Game
-</title>
-    </head>
-
-    <body>
-      <nav>
-        <a href="#">Instruction</a>
-        <a href="#">Game</a>
-      </nav>
-      <h2>Instruction</h2>
-      <main>
-        <p>
-          Concentration, also known as Match Match, Memory, Pelmanism, Shinkei-suijaku, Pexeso or simply Pairs, is a card game in which
-          all of the cards are laid face down on a surface and two cards are flipped face up over each turn. The object of the
-          game is to turn over pairs of matching cards.
-    </p>
-      </main>
-      <img src="https://image.shutterstock.com/image-vector/memory-game-preschool-children-set-260nw-785196121.jpg" alt="memory card">
-        <footer class="clearfix">
-          <p class="copyright">Copyright 2017</p>
-          <p class="message">Created with &hearts; by <span class="name">GA</span></p>
-        </footer>
-        <script src="js/main.js"></script>
-</body>
-
-</html>
+console.log("Up and running!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardthree);
+console.log("User flipped " + cardFour);
