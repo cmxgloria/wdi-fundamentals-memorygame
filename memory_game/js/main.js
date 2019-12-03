12e692e215a6082a6022abbcfdbdc9571f24a9cc
Gloria Chen <cmxgloria@gmail.com>
2019-12-03 22:09:57 +1100

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("up and runing");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
