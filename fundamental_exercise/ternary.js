72c1a1daec529637cfda4d60117fcfdb5a8d228a
Gloria Chen <cmxgloria@gmail.com>
2019-12-04 14:26:22 +1100

    update ternary samples

--- fundamental_exercise/ternary.js
+++ fundamental_exercise/ternary.js
@@ -9,3 +9,49 @@ if (coffeeIsHot === true) {
 
 isCoffeeReady;
 // => "Time to brew a new batch."
+
+var coffeeIsHot = false;
+
+var isCoffeeReady = coffeeIsHot === true ? "Drink your hot coffee!" : "Time to brew a new batch.";
+
+isCoffeeReady;
+// => "Time to brew a new batch."
+
+//another sample
+var temperature = 55;
+var typeOfExercise;
+
+if (temperature >= 45) {
+typeOfExercise = "Go for a run outside.";
+} else {
+typeOfExercise = "Go to the gym.";
+}
+
+typeOfExercise;
+// => "Go for a run outside."
+This could be shortened to the following:
+
+var temperature = 55;
+
+var typeOfExercise = temperature >= 45 ? "Go for a run outside." : "Go to the gym.";
+
+typeOfExercise;
+// => "Go for a run outside."
+
+//sample
+
+var movieCategory = "scary";
+var isMovieKidFriendly;
+
+if (movieCategory === "scary" || movieCategory === "violent") {
+isMovieKidFriendly = "Pick a different movie.";
+} else {
+isMovieKidFriendly = "Movie is kid-friendly.";
+}
+
+isMovieKidFriendly;
+// => "Pick a different movie."
+
+// var movieCategory = "scary";
+var isMovieKidFriendly = movieCategory==='scary' || movieCategory ===violent ? "Pick a different movie" : "Movie is kid-friendly. ";
+isMovieKidFriendly;
