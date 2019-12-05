726a1ce24449b6babe0f3a938706ed2ca53489c5
Gloria Chen <cmxgloria@gmail.com>
2019-12-05 22:50:48 +1100

    updated scope samples

--- fundamental_exercise/scope.js
+++ fundamental_exercise/scope.js
@@ -20,3 +20,26 @@ var orderPizzas = function(numberPeople) {
 
 orderPizzas(9);
 //You'll need to order 3 pizzas.
+
+function areBothEven(num1, num2) {
+  if (num1 % 2 === 0 && num2 % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(areBothEven(2, 4));
+//true
+
+var categorize = function(number) {
+  if (number < 8) {
+    return 8;
+  }
+  number += 3;
+  if (number < 15) {
+    return number;
+  }
+  return 100;
+};
+console.log(categorize(10));
+//13, if num is 12, will return 100, look through step by step to meet different condition, return is exiting function
