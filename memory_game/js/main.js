b0513b9f466861f9d6d30f77428b8183c8f1e943
Gloria Chen <cmxgloria@gmail.com>
2019-12-05 11:43:03 +1100

    create logic game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,14 @@
-console.log("up and runing");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = ["queen"];
+cardsInPlay.push("queen");
+console.log("User flipped queen");
+var cardTwo = ["king"];
+cardsInPlay.push("king");
+console.log("User flipped king");
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+}
+if (cardsInPlay[0] != cardsInPlay[1]) {
+  alert("Sorry, try again.");
+}
