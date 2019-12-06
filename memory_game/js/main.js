353434c6ad5a310d44f1d8c2ae2883a90d37f24b
Gloria Chen <cmxgloria@gmail.com>
2019-12-06 23:21:38 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,13 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  { rank: "queen", suit: "heart", cardImage: "images/queen-of-hearts.png" },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  { rank: "king", suit: "heart", cardImage: "images/king-of-hearts.png" },
+  { rank: "king", suit: "diamonds", cardImage: "images/king-of-diamonds.png" }
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -9,8 +18,10 @@ function checkForMatch() {
   }
 }
 function flipCard(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank);
 }
 flipCard(0);
 flipCard(2);
